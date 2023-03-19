c86f11b9328c3624b5f28a931d1088c77110d5f2
yermukhanJJ <ermuhannurkali@gmail.com>
2023-03-19 13:41:17 +0600

    fix: bag role

--- target/classes/db/migration/V5__init.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE user_score ADD COLUMN voice boolean;
-ALTER TABLE quote ADD COLUMN createAt timestamp with time zone;
-ALTER TABLE quote ADD COLUMN updateAt timestamp with time zone;
-ALTER TABLE users ADD COLUMN createAt timestamp with time zone;
